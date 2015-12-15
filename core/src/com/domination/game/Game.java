e1d26b99cc37a5216a11331310a7dcf97e785932
mrugacz95 <mrugacz95@gmail.com>
2015-12-15 11:51:43 +0100

    Game class implemented

--- core/src/com/domination/game/Game.java
+++ core/src/com/domination/game/Game.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.domination.game.gamestates.GameState;
+
+import java.util.Stack;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Stack<GameState> gameStatesStack = new Stack<GameState>();
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -18,10 +21,19 @@ public class Game extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 0.5f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+			if(!gameStatesStack.empty())
+				gameStatesStack.peek().draw();
 		batch.end();
+	}
+	public void pushGameState(GameState gameState){
+		gameState.init();
+		gameStatesStack.push(gameState);
+	}
+	public  void popGameState(){
+		gameStatesStack.peek().cleanUp();
+		gameStatesStack.pop();
 	}
 }
