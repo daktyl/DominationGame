c9eb0cdf7a6dd615abbd1b0ae35029106fe9764f
Abkaal <kacper@kurzeja.net>
2015-12-19 18:02:21 +0100

    Funkcje
    Wypełniono funkcje.

--- core/src/com/domination/game/EntityManager.java
+++ core/src/com/domination/game/EntityManager.java
@@ -12,15 +12,15 @@ public class EntityManager {
     Collection<Entity> entities;
 
     public void remove(Entity removee){
-
+        entities.remove(removee);
     }
 
     public void updateAll(){
-
+        for(Entity ent : entities) ent.update();
     }
 
     public void renderAll(){
-
+        for(Entity ent : entities) ent.render();
     }
 
     EntityManager(){
