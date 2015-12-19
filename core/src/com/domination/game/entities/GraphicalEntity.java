8a784a45006ecc22cc6029b35d5f7bb5e7d77723
Marcin Piniarski <piniarski.m@gmail.com>
2015-12-19 18:12:39 +0100

    Added GraphicalEntity class

---
+++ core/src/com/domination/game/entities/GraphicalEntity.java
@@ -0,0 +1,26 @@
+package com.domination.game.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GraphicalEntity extends Entity{
+    public Sprite sprite;
+    public GraphicalEntity(Texture texture, SpriteBatch batch) {
+        super(batch);
+        sprite = new Sprite(texture);
+    }
+
+    public GraphicalEntity(Texture texture, int startX, int startY, int width, int height, SpriteBatch batch) {
+        super(batch);
+        sprite = new Sprite(texture,startX,startY,width,height);
+    }
+
+    @Override
+    public void draw() {
+        sprite.draw(batch);
+    }
+
+    @Override
+    public void update() {} // Override this method in children classes if needed
+}
