867a6c14de57822a2cab978444e7944e94a74ecc
Abkaal <kacper@kurzeja.net>
2015-12-19 17:59:16 +0100

    Kolekcja
    Dodano kolekcję Entity.

--- core/src/com/domination/game/EntityManager.java
+++ core/src/com/domination/game/EntityManager.java
@@ -1,11 +1,19 @@
 package com.domination.game;
 
+import java.util.Collection;
+
 class Entity{
     public void update(){}
     public void render(){}
 }
 
 public class EntityManager {
+
+    Collection<Entity> entities;
+
+    public void remove(Entity removee){
+
+    }
 
     public void updateAll(){
 
