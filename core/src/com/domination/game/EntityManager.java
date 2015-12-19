c65a39b7e331dde37c0e97e6d338c6d1eb2568c3
Abkaal <kacper@kurzeja.net>
2015-12-19 17:47:48 +0100

    Entity
    Entity w celach testowych.

--- core/src/com/domination/game/EntityManager.java
+++ core/src/com/domination/game/EntityManager.java
@@ -1,9 +1,14 @@
 package com.domination.game;
 
+class Entity{
+    public void update(){}
+    public void render(){}
+}
+
 public class EntityManager {
 
     public void updateAll(){
-        
+
     }
 
     public void renderAll(){
