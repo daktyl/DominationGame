cc464bc76ad61709bde756c7cfbc2c39d3434761
Marcin Piniarski <piniarski.m@gmail.com>
2015-12-19 18:04:51 +0100

    Added Entity class

---
+++ core/src/com/domination/game/entities/Entity.java
@@ -0,0 +1,16 @@
+package com.domination.game.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class Entity {
+    public boolean isBroken;
+    public SpriteBatch batch;
+
+    public Entity(SpriteBatch batch){
+        isBroken = false;
+        this.batch = batch;
+    }
+
+    public abstract void draw();
+    public  abstract void update();
+}
