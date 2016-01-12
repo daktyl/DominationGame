1787aefa043766eea07569eb7459d1712f679fbc
Nester <fppolak@gmail.com>
2016-01-12 22:45:57 +0100

    Start button added.

--- core/src/com/domination/game/states/PlayerChooseState.java
+++ core/src/com/domination/game/states/PlayerChooseState.java
@@ -26,14 +26,14 @@ public class PlayerChooseState extends GameState {
     }
     public List<TextEntity> textList1 = new ArrayList<TextEntity>();
     private ShapeRenderer shapeRenderer = new ShapeRenderer();
-    private int menuset = 3;
+    private int menuset = 4;
     private int AISet;
-    private int AISetLeft;
+    private int AISetLeft = 1;
     private boolean isfinished = false;
     private boolean isfinishedLeft = false;
     private boolean Humanselected = true;
     private boolean AIselected = false;
-    private boolean AI1selected = false;
+    private boolean AI1selected = true;
 
     @Override
     public void init(){
@@ -48,7 +48,7 @@ public class PlayerChooseState extends GameState {
         background.sprite.setX(-background.sprite.getWidth()/2+Gdx.graphics.getWidth()/2);
         background.sprite.setY(-background.sprite.getHeight()/2+Gdx.graphics.getHeight()/2);
         ResourceManager.getInstance().get("font50");
-        TextEntity Start = new TextEntity("Press Enter to start", font50, this.batch);
+        TextEntity Start = new TextEntity("Start", font50, this.batch);
         TextEntity PlayerChoose = new TextEntity("Choose player", font50, this.batch);
         TextEntity Vs = new TextEntity("vs", font50, this.batch);
         TextEntity Com = new TextEntity("  AI  ", font50, this.batch);
@@ -59,7 +59,7 @@ public class PlayerChooseState extends GameState {
         Com.label.setPosition(Gdx.graphics.getWidth()/2 + 2*Com.label.getWidth()/2 + 110, Gdx.graphics.getHeight()/2 - Com.label.getHeight()/2 - 50);
         Human.label.setPosition(Gdx.graphics.getWidth()/2 - 2*Human.label.getWidth(), Gdx.graphics.getHeight()/2 - Human.label.getHeight()/2);
         Com1.label.setPosition(Gdx.graphics.getWidth()/2 - 2*Com1.label.getWidth() - 100, Gdx.graphics.getHeight()/2 - 5*Com1.label.getHeight()/2);
-        Start.label.setPosition(Gdx.graphics.getWidth()/2 - 2*Com1.label.getWidth(), Gdx.graphics.getHeight()/2 - 5*Com1.label.getHeight()/2 - 100);
+        Start.label.setPosition(Gdx.graphics.getWidth()/2 - 2*Com1.label.getWidth() + 160, Gdx.graphics.getHeight()/2 - 5*Com1.label.getHeight()/2 - 100);
         textList1.add(Com1);
         textList1.add(Human);
         textList1.add(Com);
@@ -81,61 +81,80 @@ public class PlayerChooseState extends GameState {
     @Override
     public boolean touchDown (int x, int y, int pointer, int button) {
         if (button == Input.Buttons.LEFT) {
-            if ((x > textList1.get(0).label.getX() - 10) && (x < textList1.get(0).label.getX() - 10 + textList1.get(0).label.getWidth() + 20) && (y > textList1.get(0).label.getY() + 245) && (y < textList1.get(0).label.getY() + textList1.get(0).label.getHeight() + 245)) {
-                menuset = 0;
-                Humanselected = false;
-                AIselected = true;
-                if (isfinished) {
-                    AISet = 0;
-                    isfinished = false;
+            if ((x > textList1.get(3).label.getX() - 10) && (x < textList1.get(3).label.getX() - 10 + textList1.get(3).label.getWidth() + 20) && (y > textList1.get(3).label.getY() + 445) && (y < textList1.get(3).label.getY() + textList1.get(0).label.getHeight() + 445)) {
+                if (Humanselected) {
+                    game.popGameState();
+                    game.pushGameState(new HumanGameplayState(game, batch, AISetLeft));
+                } else {
+                    game.popGameState();
+                    game.pushGameState(new GameplayState(game, batch, AISet, AISetLeft));
                 }
-                AISet++;
-                if (AISet == 1){
-                    textList1.get(0).label.setText("AI");
-                } else if (AISet == 2) {
-                    textList1.get(0).label.setText("FN");
-                } else if (AISet == 3) {
-                    textList1.get(0).label.setText("MM");
-                } else if (AISet == 4) {
-                    textList1.get(0).label.setText("MP");
-                } else if (AISet == 5) {
-                    textList1.get(0).label.setText("KW");
-                } else if (AISet == 6) {
-                    textList1.get(0).label.setText("AS");
-                    isfinished = true;
-                }
-            }
-            if ((x>textList1.get(1).label.getX() - 10) && (x<textList1.get(1).label.getX() - 10 + textList1.get(1).label.getWidth() + 20) && (y>textList1.get(1).label.getY()) && (y<textList1.get(1).label.getY() + textList1.get(1).label.getHeight())){
-                Humanselected = true;
-                AIselected = false;
-                menuset = 1;
-                return true;
             }
 
-            if ((x > textList1.get(2).label.getX() - 10) && (x < textList1.get(2).label.getX() - 10 + textList1.get(2).label.getWidth() + 20) && (y > textList1.get(2).label.getY() + 100) && (y < textList1.get(2).label.getY() + textList1.get(2).label.getHeight() + 100)) {
-                menuset = 2;
-                AI1selected = true;
-                if (isfinishedLeft) {
-                    AISetLeft = 0;
-                    isfinishedLeft = false;
+                if ((x > textList1.get(0).label.getX() - 10) && (x < textList1.get(0).label.getX() - 10 + textList1.get(0).label.getWidth() + 20) && (y > textList1.get(0).label.getY() + 245) && (y < textList1.get(0).label.getY() + textList1.get(0).label.getHeight() + 245)) {
+                    menuset = 0;
+                    Humanselected = false;
+                    AIselected = true;
+                    if (isfinished) {
+                        AISet = 0;
+                        isfinished = false;
+                    }
+                    AISet++;
+                    if (AISet == 1) {
+                        textList1.get(0).label.setText("AI");
+                    } else if (AISet == 2) {
+                        textList1.get(0).label.setText("FN");
+                    } else if (AISet == 3) {
+                        textList1.get(0).label.setText("MM");
+                    } else if (AISet == 4) {
+                        textList1.get(0).label.setText("MP");
+                    } else if (AISet == 5) {
+                        textList1.get(0).label.setText("KW");
+                    } else if (AISet == 6) {
+                        textList1.get(0).label.setText("AS");
+                        isfinished = true;
+                    }
                 }
-                AISetLeft++;
-                if (AISetLeft == 1){
-                    textList1.get(2).label.setText("AI");
-                } else if (AISetLeft == 2) {
-                    textList1.get(2).label.setText("FN");
-                } else if (AISetLeft == 3) {
-                    textList1.get(2).label.setText("MM");
-                } else if (AISetLeft == 4) {
-                    textList1.get(2).label.setText("MP");
-                } else if (AISetLeft == 5) {
-                    textList1.get(2).label.setText("KW");
-                } else if (AISetLeft == 6) {
-                    textList1.get(2).label.setText("AS");
-                    isfinishedLeft = true;
+                if ((x > textList1.get(1).label.getX() - 10) && (x < textList1.get(1).label.getX() - 10 + textList1.get(1).label.getWidth() + 20) && (y > textList1.get(1).label.getY()) && (y < textList1.get(1).label.getY() + textList1.get(1).label.getHeight())) {
+                    Humanselected = true;
+                    AIselected = false;
+                    menuset = 1;
+                    return true;
+                }
+
+                if ((x > textList1.get(2).label.getX() - 10) && (x < textList1.get(2).label.getX() - 10 + textList1.get(2).label.getWidth() + 20) && (y > textList1.get(2).label.getY() + 100) && (y < textList1.get(2).label.getY() + textList1.get(2).label.getHeight() + 100)) {
+                    menuset = 2;
+                    AI1selected = true;
+                    if (isfinishedLeft) {
+                        AISetLeft = 0;
+                        isfinishedLeft = false;
+                    }
+                    AISetLeft++;
+                    if (AISetLeft == 1) {
+                        textList1.get(2).label.setText("AI");
+                    } else if (AISetLeft == 2) {
+                        textList1.get(2).label.setText("FN");
+                    } else if (AISetLeft == 3) {
+                        textList1.get(2).label.setText("MM");
+                    } else if (AISetLeft == 4) {
+                        textList1.get(2).label.setText("MP");
+                    } else if (AISetLeft == 5) {
+                        textList1.get(2).label.setText("KW");
+                    } else if (AISetLeft == 6) {
+                        textList1.get(2).label.setText("AS");
+                        isfinishedLeft = true;
+                    }
                 }
             }
+            return false;
         }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        if ((screenX>textList1.get(3).label.getX() - 10) && (screenX<textList1.get(3).label.getX() - 10 + textList1.get(3).label.getWidth() + 20) && (screenY>textList1.get(3).label.getY() + 445) && (screenY<textList1.get(3).label.getY() + textList1.get(0).label.getHeight() + 445)){
+            menuset = 3;
+        }
+        else menuset = 4;
         return false;
     }
 
@@ -147,14 +166,6 @@ public class PlayerChooseState extends GameState {
                 game.popGameState();
                 Gdx.app.debug("KeyDown", "Esc");
                 return true;
-            case Input.Keys.UP:
-                menuset = 1;
-                Gdx.app.debug("KeyDown", "Up");
-                break;
-            case Input.Keys.DOWN:
-                menuset = 0;
-                Gdx.app.debug("KeyDown", "Down");
-                break;
             case Input.Keys.ENTER:
                 if (Humanselected){
                     game.popGameState();
@@ -172,7 +183,7 @@ public class PlayerChooseState extends GameState {
     @Override
     public void draw() {
         super.draw();
-        if(!(menuset == 3)) {
+        if(!(menuset == 4)) {
             shapeRenderer.begin();
             shapeRenderer.rect(textList1.get(menuset).label.getX() - 10, textList1.get(menuset).label.getY(), textList1.get(menuset).label.getWidth() + 20, textList1.get(menuset).label.getHeight());
             shapeRenderer.end();
