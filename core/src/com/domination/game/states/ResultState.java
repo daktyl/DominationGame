71bbf295edff77ea34cb1fbc87afd7e5b77def65
mrugacz95 <mrugacz95@gmail.com>
2016-01-13 23:24:47 +0100

    check Win result exit

--- core/src/com/domination/game/states/ResultState.java
+++ core/src/com/domination/game/states/ResultState.java
@@ -1,12 +1,14 @@
 package com.domination.game.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.domination.game.Game;
+import com.domination.game.engine.ResourceManager;
 import com.domination.game.entities.ButtonEntity;
 import com.domination.game.entities.TextEntity;
 import com.domination.game.players.Player;
@@ -15,7 +17,7 @@ public class ResultState extends GameState {
 
     public Player winner;
     public Player loser;
-
+    BitmapFont font;
 
     public ResultState(Player winner, Player loser, Game game, SpriteBatch batch) {
         super(game, batch);
@@ -27,15 +29,14 @@ public class ResultState extends GameState {
     @Override
     public void init() {
         setDefaultBackground();
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("ChunkfiveEx.ttf"));
-        BitmapFont font50 = generator.generateFont(50);
-        TextEntity loserText = new TextEntity("The loser is "+ loser.getPlayerName(), font50, this.batch);
+        font = ResourceManager.getInstance().get("Font50");
+        TextEntity loserText = new TextEntity("The loser is "+ loser.getPlayerName(), font, this.batch);
         entityManager.add(loserText);
         loserText.label.setPosition(300,300);
-        TextEntity Result = new TextEntity("RESULT", font50, this.batch);
+        TextEntity Result = new TextEntity("RESULT", font, this.batch);
         entityManager.add(Result);
         Result.label.setPosition(500,500);
-        TextEntity winnerText = new TextEntity("The winner is "+winner.getPlayerName(),font50, this.batch);
+        TextEntity winnerText = new TextEntity("The winner is "+winner.getPlayerName(),font, this.batch);
         entityManager.add(winnerText);
         winnerText.label.setPosition(300,400);
         ButtonEntity button = new ButtonEntity("Back",35,35,batch);
@@ -43,13 +44,16 @@ public class ResultState extends GameState {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.popGameState();
+                game.popGameState();
             }
         });
         entityManager.add(button);
     }
 
+
     @Override
     public boolean keyDown(int keycode) {
+        game.popGameState();
         game.popGameState();
         return true;
     }
